882c930a9de5aae8507e971b751de1f4152d2213
Migueleis <migueleis@hotmail.com>
2023-12-14 18:01:26 +0000

    refactor and clean day 2

--- day2/entry.js
+++ day2/entry.js
@@ -1,3 +1,8 @@
+const demo =`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 const entry =
 `Game 1: 1 green, 2 blue; 13 red, 2 blue, 3 green; 4 green, 14 red
 Game 2: 2 blue, 11 green; 4 blue, 12 red, 4 green; 7 red, 1 blue, 9 green; 10 green, 12 red, 6 blue
@@ -100,4 +105,4 @@ Game 98: 3 green, 3 blue, 2 red; 2 blue, 2 red, 1 green; 3 green, 5 blue
 Game 99: 11 green, 4 red, 12 blue; 9 red, 4 blue; 20 green, 6 blue
 Game 100: 12 red, 9 green; 12 red; 9 red, 3 green; 8 red, 4 blue, 4 green; 8 blue, 11 red, 2 green`;
 
-module.exports = entry;
+module.exports = {demo, entry};
--- day2/index.js
+++ day2/index.js
@@ -1,4 +1,4 @@
-const entry = require('./entry');
+const { demo, entry } = require('./entry');
 
 const limitMap = {
     'red': 12,
@@ -6,50 +6,76 @@ const limitMap = {
     'blue': 14,
 };
 
-const lines = entry.split('\n');
-let total = 0;
-lines.forEach((line) => {
-    const map = {
-        'red': [],
-        'green': [],
-        'blue': [],
-    };
-    console.log('line >> ', line);
-    const games = line.split(':');
-    //console.log('games >> ', games);
-    const gameId = games[0];
-    const gameValue = parseInt(gameId.split(' ')[1], 10);
-    //console.log('gameValue >> ', gameValue);
-    //console.log('gameId >> ', gameId);
-    const subsets = games[1];
-    //console.log('subsets >> ', subsets);
-    const sets = subsets.split(';');
-    sets.forEach((set) => {
-        //console.log('set >> ', set);
-        const rounds = set.split(',');
-        rounds.forEach(round => {
-            const colorValue = round.split(' ');
-            //console.log('colorValue >> ', colorValue);
-            const color = colorValue[2];
-            //console.log('color >> ', color);
-            const value = parseInt(colorValue[1], 10);
-            //console.log('value >> ', value);
-            map[color].push(value);
+const calcular = (data) => {
+    const lines = data.split('\n');
+    let total = 0;
+    lines.forEach((line) => {
+        let valid = true;
+        const games = line.split(':');
+        const gameId = games[0];
+        const gameValue = parseInt(gameId.split(' ')[1], 10);
+        const subsets = games[1];
+        const sets = subsets.split(';');
+        for (const set of sets) {
+            const rounds = set.split(',');
+            for (const round of rounds) {
+                const colorValue = round.split(' ');
+                const color = colorValue[2];
+                const value = parseInt(colorValue[1], 10);
+                if (value > limitMap[color]) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (!valid) {
+                break;
+            }
+        }
+        if (valid) {
+            total += gameValue;
+        }
+    });
+    return total;
+}
+
+const calcular2 = (data) => {
+    const lines = data.split('\n');
+    let total = 0;
+    lines.forEach((line) => {
+        const map = {
+            'red': [],
+            'green': [],
+            'blue': [],
+        };
+        const games = line.split(':');
+        const gameId = games[0];
+        const gameValue = parseInt(gameId.split(' ')[1], 10);
+        const subsets = games[1];
+        const sets = subsets.split(';');
+        sets.forEach((set) => {
+            const rounds = set.split(',');
+            rounds.forEach(round => {
+                const colorValue = round.split(' ');
+                const color = colorValue[2];
+                const value = parseInt(colorValue[1], 10);
+                map[color].push(value);
+            })
         })
-    })
-    console.log('<< reds >> ', map['red']);
-    console.log('<< greens >> ', map['green']);
-    console.log('<< blues >> ', map['blue']);
+        const maxRed = Math.max(...map['red']);
+        const maxGreen = Math.max(...map['green']);
+        const maxBlue = Math.max(...map['blue']);
+        const potencia = maxBlue * maxGreen * maxRed;
+        total += potencia
+    });
+    return total;
+}
 
-    const maxRed = Math.max(...map['red']);
-    console.log('<< maxRed >> ', maxRed);
-    const maxGreen = Math.max(...map['green']);
-    console.log('<< maxGreen >> ', maxGreen);
-    const maxBlue = Math.max(...map['blue']);
-    console.log('<< maxBlue >> ', maxBlue);
-    const potencia = maxBlue * maxGreen * maxRed;
-    console.log('<< potencia >> ', potencia);
 
-    total = total + potencia
-    console.log('total', total);
-});
+inicio = new Date();
+console.log("demo part1", calcular(demo));
+console.log("entry part1", calcular(entry));
+console.log("demo part2", calcular2(demo));
+console.log("entry part2", calcular2(entry));
+fin = new Date();
+tiempoTranscurrido = fin - inicio;
+console.log(`Tiempo transcurrido: ${tiempoTranscurrido} milisegundos`);
