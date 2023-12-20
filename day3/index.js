cbb79dfcf589d8a3fe5f0e8e212652e0db6e8c2f
Migueleis <migueleis@hotmail.com>
2023-12-20 11:02:20 +0000

    cleaned day 3

--- day3/index.js
+++ day3/index.js
@@ -1,37 +1,7 @@
-const {demo, entry} = require('./entry');
+const { demo, entry } = require('./entry');
 
-
-
-
-function encontrarNumerosAdyacentes(lineas) {
+function encontrarNumerosAdyacentes(lineas, part1) {
     let total = 0;
-
-    // Función para encontrar números adyacentes a un carácter
-    /*function encontrarNumerosDesdePosicion(i, j, caracter) {
-        let numero = caracter;
-
-        // Verificar el caracter a la izquierda
-        for (let k = j - 1; k >= 0 && /\d/.test(lineas[i][k]); k--) {
-            numero = lineas[i][k] + numero;
-        }
-
-        // Verificar el caracter a la derecha
-        for (let k = j + 1; k < lineas[i].length && /\d/.test(lineas[i][k]); k++) {
-            numero = numero + lineas[i][k];
-        }
-
-        // Verificar el caracter arriba
-        for (let k = i - 1; k >= 0 && /\d/.test(lineas[k][j]); k--) {
-            numero = lineas[k][j] + numero;
-        }
-
-        // Verificar el caracter abajo
-        for (let k = i + 1; k < lineas.length && /\d/.test(lineas[k][j]); k++) {
-            numero = numero + lineas[k][j];
-        }
-
-        return numero;
-    }*/
 
     function encontrarNumerosDesdePosicion(i, j) {
         const numerosEncontrados = new Set();
@@ -81,17 +51,24 @@ function encontrarNumerosAdyacentes(lineas) {
                 console.log("Found symbol in pos", caracter, i, j);
                 const numerosEncontrados = encontrarNumerosDesdePosicion(i, j);
                 console.log("numeros adyacentes", numerosEncontrados);
-                // Agregar a la lista final si se encontró algún número adyacente
-                if (caracter === '*' && numerosEncontrados.size === 2) {
-                    const numeros = Array.from(numerosEncontrados);
-                    const engranaje = parseInt(numeros[0], 10) * parseInt(numeros[1], 10);
-                    console.log("engranaje", engranaje);
-                    total += engranaje;
-                } /*else {
+                if (part1) {
                     for (const number of numerosEncontrados) {
                         total += parseInt(number, 10);
                     }
-                }*/
+                } else {
+                    if (caracter === '*' && numerosEncontrados.size === 2) {
+                        const numeros = Array.from(numerosEncontrados);
+                        const engranaje = parseInt(numeros[0], 10) * parseInt(numeros[1], 10);
+                        console.log("engranaje", engranaje);
+                        total += engranaje;
+                    }
+                }
+                // Agregar a la lista final si se encontró algún número adyacente
+                /*else {
+                   for (const number of numerosEncontrados) {
+                       total += parseInt(number, 10);
+                   }
+               }*/
 
             }
         }
@@ -100,25 +77,22 @@ function encontrarNumerosAdyacentes(lineas) {
     return total;
 }
 
-const lineas = entry.split('\n');
-console.log(encontrarNumerosAdyacentes(lineas));
+const part1 = (data) => {
+    const lineas = data.split('\n');
+    return encontrarNumerosAdyacentes(lineas, true);
+}
+
+const part2 = (data) => {
+    const lineas = data.split('\n');
+    return encontrarNumerosAdyacentes(lineas, false);
+}
+
 
 inicio = new Date();
-console.log("demo part1", calcular(demo));
-//console.log("entry part1", calcular(entry));
-//console.log("demo part2", calcular2(demo));
-//console.log("entry part2", calcular2(entry));
+console.log("demo part1", part1(demo));
+console.log("entry part1", part1(entry));
+console.log("demo part2", part2(demo));
+console.log("entry part2", part2(entry));
 fin = new Date();
 tiempoTranscurrido = fin - inicio;
 console.log(`Tiempo transcurrido: ${tiempoTranscurrido} milisegundos`);
-
-/*let total = 0;
-let firstLine = lines[0];
-for (let i = 1; i < lines.length; i++) {
-
-    const secondLine = lines[i];
-    console.log('firstLine >> ', firstLine);
-    console.log('secondLine >> ', secondLine);
-    firstLine = secondLine;
-
-}*/
