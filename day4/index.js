03b4defe1b0ea5d9c07791f3e9fd650fef235ad8
Migueleis <migueleis@hotmail.com>
2023-12-20 16:20:36 +0000

    day 4 cleaned

--- day4/index.js
+++ day4/index.js
@@ -1,37 +1,24 @@
-const entry = require('./entry');
-
-const lineas = entry.split('\n');
+const { demo, entry } = require('./entry');
 
 const numerosCoincidentes = (linea) => {
     const premios = [];
-    //console.log("linea:", linea);
     const tarjeta = linea.split(':');
-    //console.log("tarjeta number:", tarjeta[0]);
-    //console.log("numeros:", tarjeta[1]);
     const numeros = tarjeta[1].split('|');
-    //console.log("numerosGanadores:", numeros[0]);
-    // console.log("numerosJugados:", numeros[1]);
     const numerosGanadores = numeros[0].trim().split(/\s+/).map(Number);
     const numerosJugados = numeros[1].trim().split(/\s+/).map(Number);
     numerosGanadores.forEach(numg => {
-        //console.log("num:", numg);
         numerosJugados.forEach(numj => {
-            //console.log("num:", numj);
             if (numg === numj) {
                 premios.push(numg);
             }
         })
     })
-    //console.log("premios:", premios);
     const aciertos = premios.length;
-   // console.log("aciertos:", aciertos);
     return aciertos;
 }
 
-const calcularPuntos = (lineas) => {
+const part2 = (lineas) => {
     let total = new Array(198).fill(1);
-
-    // lineas.forEach(linea => {
     for (let i = 0; i < lineas.length; i++) {
         for (let cards = 0; cards < total[i]; cards++) {
             let aciertos = numerosCoincidentes(lineas[i]);
@@ -39,16 +26,26 @@ const calcularPuntos = (lineas) => {
                 total[i + aciertos] += 1;
                 aciertos--;
             }
-           // console.log("total:", total);
         }
     }
-
     const suma = total.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
-    console.log("suma:", suma);
     return suma;
+}
+
+const part1 = (lineas) => {
+    let total = 0;
+    for (let i = 0; i < lineas.length; i++) {
+        let aciertos = numerosCoincidentes(lineas[i]);
+        if(aciertos>0){
+            const cardValue = aciertos === 1 ? 1 : Math.pow(2, aciertos - 1);
+            total += cardValue;
+        }
+    }
+    return total;
 }
 
 
 
-
-console.log('total', calcularPuntos(lineas));
+const lineas = entry.split('\n');
+console.log('part1', part1(lineas));
+console.log('part2', part2(lineas));
