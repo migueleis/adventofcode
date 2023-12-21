5ff39fd2f317a0e52c56b24a0131a9f22e7a78e7
Migueleis <migueleis@hotmail.com>
2023-12-21 17:06:35 +0000

    day 6 cleaned

--- day6/index.js
+++ day6/index.js
@@ -52,31 +52,8 @@ const calcular2 = () => {
     return count;
 }
 
-
-function contarCombinacionesMayoresQueX(targetSum, limite) {
-    let contador = 0;
-
-    for (let num1 = 1; num1 <= targetSum / 2; num1++) {
-        const num2 = targetSum - num1;
-
-        if (num2 >= num1 && num2 > limite) {
-            contador++;
-        }
-    }
-
-    return contador;
-}
-
 let inicio = new Date();
-console.log(contarCombinacionesMayoresQueX(71530, 940200));
+console.log('salida', calcular2());
 let fin = new Date();
 let tiempoTranscurrido = fin - inicio;
-console.log(`Tiempo transcurrido: ${tiempoTranscurrido} milisegundos`);
-
-
-
-inicio = new Date();
-console.log('salida', calcular2());
-fin = new Date();
-tiempoTranscurrido = fin - inicio;
 console.log(`Tiempo transcurrido: ${tiempoTranscurrido} milisegundos`);
